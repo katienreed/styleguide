8b14692eefb18bc722a01d6a0c9a274385aa3a2f
Katie Reed <katie.reed@namely.com>
2015-10-14 11:48:13 -0400

    Remove imports of Select children

--- src/js/pages/forms.js
+++ src/js/pages/forms.js
@@ -10,9 +10,6 @@ import SimpleSelect from '../components/forms/fields/simple-select';
 import EditLabel from '../components/edit-label';
 import FileInput from '../components/forms/file-input';
 import Select from '../components/forms/select/select';
-import SingleValue from '../components/forms/select/single-value';
-import Value from '../components/forms/select/value';
-import Option from '../components/forms/select/option';
 
 
 
