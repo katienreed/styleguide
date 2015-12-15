54b0cb3c1d544d84f5f34991351ea4a50eb2f559
Katie Reed <katie.reed@namely.com>
2015-12-15 10:42:20 -0500

    Add spec file for action bar

---
+++ test/view/bars/action-bar-spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import TestUtils from 'react/lib/ReactTestUtils';
+import React from 'react';
+import ActionBar from 'view/bars/action-bar';
+
+describe('ActionBar', () => {
+
+  // it('expects ---', () => {
+  //
+  // });
+
+});
