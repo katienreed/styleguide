375eb8f7416d05bd62ae1f2667c3902e37460261
Katie Reed <katie.reed@namely.com>
2015-12-17 13:37:36 -0500

    Add default props for action bar, create test

--- test/view/view.spec.js
+++ test/view/view.spec.js
@@ -6,45 +6,45 @@ import View from 'view/view';
 describe('View', () => {
   describe('#actionBar()', () => {
 
-    // should I pass in example props? and then test said props?
-    // ^^ is this what shallow rendering is?
 
     it('returns null if this.state.bars.action.use is null', () => {
       let component = TestUtils.renderIntoDocument(<View />)
       expect(component.actionBar()).to.be.null
     })
 
-    // test to make sure the component renders
-    it('renders the component if called to the view', () => {
+    it('returns null if this.state.bars.action.visible is false', () => {
       let component = TestUtils.renderIntoDocument(<View />)
+      component.setState({bars: {action: { use: true, visible: false}}})
+      expect(component.actionBar()).to.be.null
+    })
 
+    it('returns null if this.state.bars.action.visible is false', () => {
+      let component = TestUtils.renderIntoDocument(<View />)
+      component.setState({bars: {action: { use: true, visible: true}}})
+      expect(component.actionBar()).to.be.ok
     })
 
     // test for the action buttons to appear if the length is greater than one
+
 
     // test for the component to be mounted
 
     // test for the component to be unmounted
 
     // test to make sure the title renders correctly
-    it('#title()', () => {
-      let simple_select = TestUtils.renderIntoDocument(<SimpleSelect title={this.props.title}/>)
-      expect(this.props.title.length).to.equal(1)
-    })
-
-    // test to make sure the description renders correctly
-    it('#description()', () => {
-        TestUtils.findRenderedDOMComponentWithClass(
-          render({ type: 'text' }), 'fake-Text'
-        );
-      });
+    // it('#title()', () => {
+    //   let simple_select = TestUtils.renderIntoDocument(<SimpleSelect title={this.props.title}/>)
+    //   expect(this.props.title.length).to.equal(1)
+    // })
+    //
+    // // test to make sure the description renders correctly
+    // it('#description()', () => {
+    //     TestUtils.findRenderedDOMComponentWithClass(
+    //       render({ type: 'text' }), 'fake-Text'
+    //     );
+    //   });
 
     // test inline styles in functions() - e.g. title, description, render?
 
-    it('returns null if this.state.bars.action.visible is false', () => {
-      let component = TestUtils.renderIntoDocument(<View />)
-      component.setState({bars: {action: { use: true, visible: false}}})
-      expect(component.actionBar()).to.be.null
-    })
   })
 });
