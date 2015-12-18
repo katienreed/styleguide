b2377842b6cdd6a7ca1d3ad0ef30580d1ae362d2
Katie Reed <katie.reed@namely.com>
2015-12-18 11:26:21 -0500

    Add two more tests

--- test/view/view.spec.js
+++ test/view/view.spec.js
@@ -2,10 +2,10 @@ import { expect } from 'chai';
 import TestUtils from 'react/lib/ReactTestUtils';
 import React from 'react';
 import View from 'view/view';
+import ActionBar from 'view/bars/action-bar';
 
 describe('View', () => {
   describe('#actionBar()', () => {
-
 
     it('returns null if this.state.bars.action.use is null', () => {
       let component = TestUtils.renderIntoDocument(<View />)
@@ -24,27 +24,23 @@ describe('View', () => {
       expect(component.actionBar()).to.be.ok
     })
 
-    // test for the action buttons to appear if the length is greater than one
+  })
 
+  describe('ActionBar', () => {
+    describe('#title()', () => {
+      it('renders the prop title',() => {
+        let component = TestUtils.renderIntoDocument(<ActionBar title={'Test Title'} />)
+        let elem = TestUtils.scryRenderedDOMComponentsWithTag(component, 'h3')
+        expect(elem.length).to.equal(1)
+      })
+    })
 
-    // test for the component to be mounted
-
-    // test for the component to be unmounted
-
-    // test to make sure the title renders correctly
-    // it('#title()', () => {
-    //   let simple_select = TestUtils.renderIntoDocument(<SimpleSelect title={this.props.title}/>)
-    //   expect(this.props.title.length).to.equal(1)
-    // })
-    //
-    // // test to make sure the description renders correctly
-    // it('#description()', () => {
-    //     TestUtils.findRenderedDOMComponentWithClass(
-    //       render({ type: 'text' }), 'fake-Text'
-    //     );
-    //   });
-
-    // test inline styles in functions() - e.g. title, description, render?
-
+    describe('#description()', () => {
+      it('renders the prop description',() => {
+        let component = TestUtils.renderIntoDocument(<ActionBar description={'Test Description'} />)
+        let elem = TestUtils.scryRenderedDOMComponentsWithClass(component, 'flex-auto right-align')
+        expect(elem.length).to.equal(1)
+        })
+    })
   })
 });
